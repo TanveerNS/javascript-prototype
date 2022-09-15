a7775b0196c465736c695de4c75eb2787b704bb4
tanveerns <tanveeralam229@gmail.com>
2022-09-15 21:29:33 +0530

    Array-reduce

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-Array.prototype.newFilter = function (callback, context) {
-  const result = [];
+Array.prototype.newReduce = function (callback, startingValue) {
+  // as starting value is an optional param
+  // make a check
+  let accumulator = startingValue || undefined;
   for (let index = 0; index < this.length; index++) {
-    if (callback.call(context, this[index], index, this)) {
-      result.push(this[index])
+    if (accumulator) {
+      accumulator = callback.call(accumulator, accumulator, this[index], index, this)
+    } else {
+      accumulator = this[index]
     }
   }
-  return result
+  return accumulator;
 }
-// example
-const numbers = [1, 2, 3, 4]
-const even = numbers.newFilter(item => item % 2 === 0)
-console.log(even)
+const nums = [1, 2, 3, 4, 5]
+const double = nums.newReduce((accum, current) => {
+  accum.push(current * 2)
+  return accum
+}, []);
+console.log(double)
+const queryString = "cat=meow&duck=quack&dog=woof";
+const queryObject = queryString.split("&").newReduce((accum, current) => {
+  const splitString = current.split("=")
+  accum[splitString[0]] = splitString[1];
+  return accum;
+}, {})
+console.log(queryObject);
