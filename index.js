5a6f7fddb3669ecce8826a70b9e53068e068a021
tanveerns <tanveeralam229@gmail.com>
2022-09-15 21:28:17 +0530

    Array-filter

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-Array.prototype.newMap = function (callback) {
-    const result = [];
-    for (let index = 0; index < this.length; index++) {
-      // This is primarily to check if the item
-      // exists in the array, 
-      if (this.indexOf(this[index]) > -1) {
-        result[index] = callback(this[index], index, this)
-      }
+Array.prototype.newFilter = function (callback, context) {
+  const result = [];
+  for (let index = 0; index < this.length; index++) {
+    if (callback.call(context, this[index], index, this)) {
+      result.push(this[index])
     }
-    return result
   }
-  // example
-  const numbers = [1, 2, 3, 4]
-  numbers[10] = 34;
-  const double = numbers.newMap((item, index) => {
-    return item * 2
-  })
-  console.log(double)
+  return result
+}
+// example
+const numbers = [1, 2, 3, 4]
+const even = numbers.newFilter(item => item % 2 === 0)
+console.log(even)
