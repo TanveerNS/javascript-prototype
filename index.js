05c2d68ff44858e196205645c25d1f9bcda69266
tanveerns <tanveeralam229@gmail.com>
2022-09-16 21:52:22 +0530

    Array-foreach

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
-Array.prototype.newReduce = function (callback, startingValue) {
-  // as starting value is an optional param
-  // make a check
-  let accumulator = startingValue || undefined;
+Array.prototype.newForEach = function (callback, context) {
   for (let index = 0; index < this.length; index++) {
-    if (accumulator) {
-      accumulator = callback.call(accumulator, accumulator, this[index], index, this)
-    } else {
-      accumulator = this[index]
+    // This is primarily to check if the item
+    // exists in the array, 
+    if (this.indexOf(this[index]) > -1) {
+      callback.call(context, this[index], index, this)
     }
   }
-  return accumulator;
 }
-const nums = [1, 2, 3, 4, 5]
-const double = nums.newReduce((accum, current) => {
-  accum.push(current * 2)
-  return accum
-}, []);
-console.log(double)
-const queryString = "cat=meow&duck=quack&dog=woof";
-const queryObject = queryString.split("&").newReduce((accum, current) => {
-  const splitString = current.split("=")
-  accum[splitString[0]] = splitString[1];
-  return accum;
-}, {})
-console.log(queryObject);
+// example
+const words = ["adam", "ate", "an", "apple"]
+const upperCaseList = []
+words.newForEach((word, index, context) => {
+  upperCaseList.push(word.toUpperCase())
+})
+console.log(upperCaseList)
