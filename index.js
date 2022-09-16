2bce1ba4c16c5c6e9c69b44db2866bdef0b2b5e9
tanveerns <tanveeralam229@gmail.com>
2022-09-16 21:53:24 +0530

    function-bind

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-Array.prototype.newForEach = function (callback, context) {
-  for (let index = 0; index < this.length; index++) {
-    // This is primarily to check if the item
-    // exists in the array, 
-    if (this.indexOf(this[index]) > -1) {
-      callback.call(context, this[index], index, this)
-    }
+Function.prototype.newBind = function (context) {
+  const currentContext = this;
+  const currentArguments = Array.prototype.slice.call(arguments, 1); // Dont need the context
+  return function () {
+    const args = Array.prototype.slice.call(arguments);
+    currentContext.apply(context, currentArguments.concat(args))
   }
 }
-// example
-const words = ["adam", "ate", "an", "apple"]
-const upperCaseList = []
-words.newForEach((word, index, context) => {
-  upperCaseList.push(word.toUpperCase())
-})
-console.log(upperCaseList)
+this.x = 9;    // this refers to global "window" object here in the browser
+const module = {
+  x: 81,
+  getX: function () { return this.x; }
+};
+module.getX(); // 81
+const retrieveX = module.getX;
+retrieveX();
+// returns 9 - The function gets invoked at the global scope
+// Create a new function with 'this' bound to module
+// New programmers might confuse the
+// global const x with module's property x
+const boundGetX = retrieveX.bind(module);
+console.log(boundGetX());
