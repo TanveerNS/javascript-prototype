6eae229f660c948ab649d91cd7632d84e3f7f437
tanveerns <tanveeralam229@gmail.com>
2022-09-19 22:23:21 +0530

    function asyncParallel(taskList, resultsCallback) {

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
-Function.prototype.newBind = function (context) {
-  const currentContext = this;
-  const currentArguments = Array.prototype.slice.call(arguments, 1); // Dont need the context
-  return function () {
-    const args = Array.prototype.slice.call(arguments);
-    currentContext.apply(context, currentArguments.concat(args))
-  }
+function asyncParallel(taskList, resultsCallback) {
+  const results = [];
+  let tasksCompleted = 0;
+  taskList.forEach(asyncTask => {
+    asyncTask(value => {
+      results.push(value);
+      tasksCompleted++;
+      if (tasksCompleted >= taskList.length) {
+        resultsCallback.call(null, results);
+      }
+    });
+  });
 }
-this.x = 9;    // this refers to global "window" object here in the browser
-const module = {
-  x: 81,
-  getX: function () { return this.x; }
-};
-module.getX(); // 81
-const retrieveX = module.getX;
-retrieveX();
-// returns 9 - The function gets invoked at the global scope
-// Create a new function with 'this' bound to module
-// New programmers might confuse the
-// global const x with module's property x
-const boundGetX = retrieveX.bind(module);
-console.log(boundGetX());
+const taskList = [
+  createAsyncTask(),
+  createAsyncTask(),
+  createAsyncTask(),
+  createAsyncTask(),
+  createAsyncTask(),
+  createAsyncTask()
+];
+asyncParallel(taskList, result => {
+  console.log('got the results', result);
+});
