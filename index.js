884d6225c69e9791ef2cfd9f387c51d2a48b8ad2
tanveerns <tanveeralam229@gmail.com>
2022-09-28 21:48:24 +0530

    array-flatten

--- index.js
+++ index.js
@@ -1,32 +1,18 @@
-function task(time) {
-  return new Promise(function (resolve, reject) {
-    setTimeout(function () {
-      resolve(time);
-    }, time);
-  });
+Array.prototype.flatten = function () {
+  let flatArray = []
+  for (let index = 0; index < this.length; index++) {
+    const element = this[index];
+    if (Array.isArray(element)) {
+      flatArray = flatArray.concat(this.flatten.call(element))
+    } else {
+      flatArray.push(element)
+    }
+  }
+  return flatArray;
 }
-const taskList = [task(1000), task(5000), task(3000)];
-// returns promise with results in an array
-function myPromiseAll(taskList) {
-  const results = []
-  let promisesCompleted = 0;
-  return new Promise((resolve, reject) => {
-    taskList.forEach((promise, index) => {
-      promise.then((val) => {
-        results[index] = val;
-        promisesCompleted += 1;
-        if (promisesCompleted === taskList.length) {
-          resolve(results)
-        }
-      })
-        .catch(error => {
-          reject(error)
-        })
-    })
-  });
-}
-myPromiseAll(taskList)
-  .then(results => {
-    console.log("got results", results)
-  })
-  .catch(console.error)
+// function flatten
+//const nestedArr = [[1], [[1, 4, [5, 3]], [1, 2, 3, 4]]] => [1, 1, 4, 5, 3, 1, 2, 3, 4]
+const nestedArr = [[1], [[1, 4, [5, 3]], [1, 2, 3, [3, 4, [2, [22, [3, 4, 5, 6, 5, [2]]]]], 4]]]
+//const nestedArr = [1, 2, 3, 4, [1]]
+const flat = nestedArr.flatten();
+console.log(flat);
